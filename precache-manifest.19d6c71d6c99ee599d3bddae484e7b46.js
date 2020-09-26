3fb77c5618884d294336bbf8918490ffbce7bbb9
rohitpim1 <pimrohit33@gmail.com>
2020-09-26 20:47:33 +0530

    Updates

---
+++ precache-manifest.19d6c71d6c99ee599d3bddae484e7b46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fffb97550574ba3403cac0884d7abac",
+    "url": "/Digitalclockreact/index.html"
+  },
+  {
+    "revision": "30895f8eea49eda67600",
+    "url": "/Digitalclockreact/static/css/main.dadf0b7a.chunk.css"
+  },
+  {
+    "revision": "e995b0bce46043468a9a",
+    "url": "/Digitalclockreact/static/js/2.5e167b9f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Digitalclockreact/static/js/2.5e167b9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30895f8eea49eda67600",
+    "url": "/Digitalclockreact/static/js/main.ae678199.chunk.js"
+  },
+  {
+    "revision": "0cd5a5dd97aeffb94557",
+    "url": "/Digitalclockreact/static/js/runtime-main.beff4fcd.js"
+  }
+]);
